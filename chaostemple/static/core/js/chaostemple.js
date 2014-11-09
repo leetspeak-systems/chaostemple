24aea5e286dc07131b2ecc6dbf2993ee08a22a0d
Helgi Hrafn Gunnarsson <helgi@binary.is>
2014-11-09 19:14:46 +0000

    Oops, this stuff belonged in the last commit but apparently wasn't included

--- chaostemple/static/core/js/chaostemple.js
+++ chaostemple/static/core/js/chaostemple.js
@@ -1,4 +1,26 @@
 
+// Generalized function for handling JSON on our site
+$.jsonize = function(url, data, done, error) {
+    result = $.get(
+        url,
+        data,
+        dataType = 'json'
+    ).done(function(data, textStatus) {
+        data = $.parseJSON(data);
+        if (data.ok) {
+            done(data, textStatus);
+        }
+        else {
+            alert('Server error: ' + data.error);
+            if (error != null) {
+                error(data, textStatus);
+            }
+        }
+    });
+    return result;
+}
+
+// Set focus on first text field on page, if it exists
 $(document).ready(function() {
     $("form input[type='text']").focus();
 });

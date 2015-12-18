e3ac6c0192678b233e71838121e4246982403141
Helgi Hrafn Gunnarsson <helgi@binary.is>
2015-12-18 14:05:49 +0000

    Bugfix: SITE_ID added to settings.py to prevent error

--- chaostemple/chaostemple/settings.py
+++ chaostemple/chaostemple/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+SITE_ID = 1
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/

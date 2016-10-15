9437d40650113839af12f2a447bf6ba9ca322f69
Helgi Hrafn Gunnarsson <helgi@binary.is>
2016-10-15 14:53:00 -0400

    Improvements to displaying of timing of sessions and committee agendas.

---
+++ chaostemple/core/templatetags/committee.py
@@ -0,0 +1,19 @@
+from django import template
+from django.template.defaultfilters import date
+from django.template.defaultfilters import time
+
+register = template.Library()
+
+@register.filter
+def fancy_committee_agenda_timing(agenda, skipdate=False):
+    '''Display appropriate timing of committee agenda depending on context and available information'''
+
+    timing = agenda.timing_start_planned
+
+    if timing.time().isoformat() != '00:00:00':
+        return timing if not skipdate else time(timing)
+    elif agenda.timing_text:
+        return '%s (%s)' % (date(timing), agenda.timing_text) if not skipdate else agenda.timing_text
+    else:
+        return date(timing) if not skipdate else ''
+
---
+++ chaostemple/core/templatetags/session.py
@@ -0,0 +1,20 @@
+from django import template
+from django.template.defaultfilters import capfirst
+from django.template.defaultfilters import date
+from django.template.defaultfilters import time
+
+register = template.Library()
+
+@register.filter
+def fancy_session_timing(session, skipdate=False):
+    '''Display appropriate timing of session depending on context and available information'''
+
+    timing = session.timing_start_planned
+
+    if timing:
+        return timing if not skipdate else time(timing)
+    elif session.timing_text:
+        return capfirst(session.timing_text)
+    else:
+        return ''
+

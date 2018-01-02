aec493e19b33a5d8dc59cb29f28d798323d9fe19
Helgi Hrafn Gunnarsson <helgi@binary.is>
2018-01-02 00:23:08 +0000

    Althingi module tests: Function decorator added for suppressing output, so that we don't have to use "with HiddenPrints" with the extra indentation.

--- chaostemple/althingi/tests.py
+++ chaostemple/althingi/tests.py
@@ -42,12 +42,20 @@ class HiddenPrints:
     def __exit__(self, exc_type, exc_val, exc_tb):
         sys.stdout = self._original_stdout
 
+# A function decorator for hiding prints.
+def hidden_prints(test_function):
+    def wrapped(*args, **kwargs):
+        with HiddenPrints():
+            return test_function(*args, **kwargs)
+    return wrapped
+
 
 class AlthingiUpdaterTest(TestCase):
 
     def setUp(self):
         clear_already_haves()
 
+    @hidden_prints
     def test_update_parliament(self):
         '''
         Any updater function that takes parliament_num as a parameter will
@@ -57,62 +65,58 @@ class AlthingiUpdaterTest(TestCase):
 
         parliament_num = get_last_parliament_num()
 
-        with HiddenPrints():
+        # Pass: Basic usage
+        parliament = update_parliament(parliament_num)
+        self.assertEquals(parliament.parliament_num, parliament_num)
 
-            # Pass: Basic usage
-            parliament = update_parliament(parliament_num)
-            self.assertEquals(parliament.parliament_num, parliament_num)
+        # Pass: Figure out default parliament_num
+        parliament = update_parliament(None)
+        self.assertEquals(parliament.parliament_num, parliament_num)
 
-            # Pass: Figure out default parliament_num
-            parliament = update_parliament(None)
-            self.assertEquals(parliament.parliament_num, parliament_num)
+        # Fail: Good number passed as string
+        self.assertRaises(TypeError, update_parliament, str(parliament_num))
 
-            # Fail: Good number passed as string
-            self.assertRaises(TypeError, update_parliament, str(parliament_num))
+        # Fail: Get a parliament before the first one
+        self.assertRaises(AlthingiException, update_parliament, FIRST_PARLIAMENT_NUM - 1)
 
-            # Fail: Get a parliament before the first one
-            self.assertRaises(AlthingiException, update_parliament, FIRST_PARLIAMENT_NUM - 1)
+        # Fail: Get a parliament from the (apparent) future
+        self.assertRaises(AlthingiException, update_parliament, parliament_num + 1)
 
-            # Fail: Get a parliament from the (apparent) future
-            self.assertRaises(AlthingiException, update_parliament, parliament_num + 1)
-
+    @hidden_prints
     def test_update_persons(self):
-        with HiddenPrints():
-            persons = update_persons()
+        persons = update_persons()
 
+    @hidden_prints
     def test_update_person(self):
 
-        with HiddenPrints():
+        # Fail: Good number passed as something else than a number.
+        person_xml_id, parliament_num, name = main_test_dummy
+        with self.assertRaisesRegexp(TypeError, 'Parameter person_xml_id must be a number'):
+            update_person(str(person_xml_id), parliament_num)
 
-            # Fail: Good number passed as something else than a number.
-            person_xml_id, parliament_num, name = main_test_dummy
-            with self.assertRaisesRegexp(TypeError, 'Parameter person_xml_id must be a number'):
-                update_person(str(person_xml_id), parliament_num)
+        # Fail: Fetch a person that we know does not exist.
+        person_xml_id, parliament_num, name = broken_test_dummy
+        with self.assertRaisesRegexp(AlthingiException, 'Person with XML-ID \d+ not found'):
+            update_person(person_xml_id, parliament_num)
 
-            # Fail: Fetch a person that we know does not exist.
-            person_xml_id, parliament_num, name = broken_test_dummy
-            with self.assertRaisesRegexp(AlthingiException, 'Person with XML-ID \d+ not found'):
-                update_person(person_xml_id, parliament_num)
+        # Pass: Iterate through our test dummies and check their names.
+        for person_xml_id, parliament_num, name in test_dummies:
+            person = update_person(person_xml_id, parliament_num)
+            self.assertEquals(person.name, name)
 
-            # Pass: Iterate through our test dummies and check their names.
-            for person_xml_id, parliament_num, name in test_dummies:
-                person = update_person(person_xml_id, parliament_num)
-                self.assertEquals(person.name, name)
-
+    @hidden_prints
     def test_update_seats(self):
 
-        with HiddenPrints():
+        # Fail: Good number passed as something else than a number.
+        person_xml_id, parliament_num, name = main_test_dummy
+        with self.assertRaisesRegexp(TypeError, 'Parameter person_xml_id must be a number'):
+            update_seats(str(person_xml_id), parliament_num)
 
-            # Fail: Good number passed as something else than a number.
-            person_xml_id, parliament_num, name = main_test_dummy
-            with self.assertRaisesRegexp(TypeError, 'Parameter person_xml_id must be a number'):
-                update_seats(str(person_xml_id), parliament_num)
-
-            # Fail: Fetch seats for a person that does not exist.
-            person_xml_id, parliament_num, name = broken_test_dummy
-            with self.assertRaisesRegexp(AlthingiException, 'Person with XML-ID \d+ not found'):
-                update_seats(person_xml_id, parliament_num)
-
-            # Pass: Fetch the seats of a person known to exist.
-            person_xml_id, parliament_num, name = main_test_dummy
+        # Fail: Fetch seats for a person that does not exist.
+        person_xml_id, parliament_num, name = broken_test_dummy
+        with self.assertRaisesRegexp(AlthingiException, 'Person with XML-ID \d+ not found'):
             update_seats(person_xml_id, parliament_num)
+
+        # Pass: Fetch the seats of a person known to exist.
+        person_xml_id, parliament_num, name = main_test_dummy
+        update_seats(person_xml_id, parliament_num)

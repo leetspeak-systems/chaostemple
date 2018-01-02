008406293b9622245b8607afa13231d48c3925f6
Helgi Hrafn Gunnarsson <helgi@binary.is>
2018-01-02 03:48:38 +0000

    Althingi module tests: Dummy data simplified for simplicity and speed.

--- chaostemple/althingi/tests.py
+++ chaostemple/althingi/tests.py
@@ -17,42 +17,12 @@ from althingi.updaters import update_vote_castings
 from althingi.utils import get_last_parliament_num
 
 
-# Test dummies from different parliaments for testing known, valid data.
-test_dummies = (
-    (1166, 148, u'Helgi Hrafn Gunnarsson'),
-    (1251, 147, u'Pawel Bartoszek'),
-    (1261, 146, u'Áslaug Arna Sigurbjörnsdóttir'),
-    (1012, 145, u'Bjarkey Olsen Gunnarsdóttir'),
-    (1214, 144, u'Ásta Guðrún Helgadóttir'),
-    (1168, 143, u'Jón Þór Ólafsson'),
-    ( 683, 142, u'Guðbjartur Hannesson'),
-    ( 287, 141, u'Jóhanna Sigurðardóttir'),
-    (1138, 140, u'Amal Tamimi'),
-)
-
-# Sometimes we just need one test dummy.
-main_test_dummy = test_dummies[0]
-
-# An additional test dummy known to not exist.
+# Test dummies.
+test_dummy = (1166, 148, u'Helgi Hrafn Gunnarsson')
 broken_test_dummy = (3, 148, 'Broken Test Dummy')
 
-# Vote castings known to be valid.
-test_dummy_vote_castings = (
-    (54805, 148),
-    (54799, 147),
-    (53954, 146),
-    (51984, 145),
-    (50400, 144),
-    (48958, 143), # Note: No actual votes, speaker declaration.
-    (48871, 142),
-    (47192, 141),
-    (45412, 140),
-)
-
-# Main test dummy vote casting
-main_test_dummy_vote_casting = test_dummy_vote_castings[0]
-
-# Vote casting known to be invalid.
+# Test dummy vote castings.
+test_dummy_vote_casting = (54805, 148)
 broken_test_dummy_vote_casting = (1, 148)
 
 
@@ -127,7 +97,7 @@ class AlthingiUpdaterTest(TestCase):
     def test_update_person(self):
 
         # Fail: Good number passed as something else than a number.
-        person_xml_id, parliament_num, name = main_test_dummy
+        person_xml_id, parliament_num, name = test_dummy
         with self.assertRaisesRegexp(TypeError, 'Parameter person_xml_id must be a number'):
             update_person(str(person_xml_id), parliament_num)
 
@@ -136,16 +106,16 @@ class AlthingiUpdaterTest(TestCase):
         with self.assertRaisesRegexp(AlthingiException, 'Person with XML-ID \d+ not found'):
             update_person(person_xml_id, parliament_num)
 
-        # Pass: Iterate through our test dummies and check their names.
-        for person_xml_id, parliament_num, name in test_dummies:
-            person = update_person(person_xml_id, parliament_num)
-            self.assertEquals(person.name, name)
+        # Pass: Fetch a person known to exist.
+        person_xml_id, parliament_num, name = test_dummy
+        person = update_person(person_xml_id, parliament_num)
+        self.assertEquals(person.name, name)
 
     @hidden_prints
     def test_update_seats(self):
 
         # Fail: Good number passed as something else than a number.
-        person_xml_id, parliament_num, name = main_test_dummy
+        person_xml_id, parliament_num, name = test_dummy
         with self.assertRaisesRegexp(TypeError, 'Parameter person_xml_id must be a number'):
             update_seats(str(person_xml_id), parliament_num)
 
@@ -155,7 +125,7 @@ class AlthingiUpdaterTest(TestCase):
             update_seats(person_xml_id, parliament_num)
 
         # Pass: Fetch the seats of a person known to exist.
-        person_xml_id, parliament_num, name = main_test_dummy
+        person_xml_id, parliament_num, name = test_dummy
         update_seats(person_xml_id, parliament_num)
 
     @hidden_prints
@@ -166,7 +136,7 @@ class AlthingiUpdaterTest(TestCase):
     def test_update_vote_casting(self):
 
         # Fail: Good number passed as something else than number.
-        vote_casting_xml_id, parliament_num = main_test_dummy_vote_casting
+        vote_casting_xml_id, parliament_num = test_dummy_vote_casting
         with self.assertRaisesRegexp(TypeError, 'Parameter vote_casting_xml_id must be a number'):
             update_vote_casting(str(vote_casting_xml_id), parliament_num)
 
@@ -175,15 +145,15 @@ class AlthingiUpdaterTest(TestCase):
         with self.assertRaisesRegexp(AlthingiException, 'Vote casting \d+ does not exist'):
             update_vote_casting(vote_casting_xml_id, parliament_num)
 
-        # Pass: Fetch some vote castings known to be valid.
-        for vote_casting_xml_id, parliament_num in test_dummy_vote_castings:
-            update_vote_casting(vote_casting_xml_id, parliament_num)
+        # Pass: Fetch a vote casting known to be valid.
+        vote_casting_xml_id, parliament_num = test_dummy_vote_casting
+        update_vote_casting(vote_casting_xml_id, parliament_num)
 
     @hidden_prints
     def test_update_committee_seats(self):
 
         # Fail: Good number passed as something else than number.
-        person_xml_id, parliament_num, name = main_test_dummy
+        person_xml_id, parliament_num, name = test_dummy
         with self.assertRaisesRegexp(TypeError, 'Parameter person_xml_id must be a number'):
             update_committee_seats(str(person_xml_id), parliament_num)
 
@@ -193,5 +163,5 @@ class AlthingiUpdaterTest(TestCase):
             update_committee_seats(person_xml_id, parliament_num)
 
         # Pass: Fetch committee seats for a person known to be valid.
-        person_xml_id, parliament_num, name = main_test_dummy
+        person_xml_id, parliament_num, name = test_dummy
         update_committee_seats(person_xml_id, parliament_num)

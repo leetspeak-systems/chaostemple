edb713f3f2da65a297bb69ff4879537774fda368
Helgi Hrafn Gunnarsson <helgi@binary.is>
2018-03-16 15:30:48 +0000

    Partial functionality: MPs automatically added to groups reflecting their party. Groups are created as needed and given full access.

--- chaostemple/customsignup/signals.py
+++ chaostemple/customsignup/signals.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.models import Group
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from althingi.models import Person
 
+from core.models import Access
 from core.models import UserProfile
 
 from django.contrib.auth.models import User
@@ -10,7 +12,7 @@ from django.contrib.auth.models import User
 
 @receiver(post_save, sender=Person)
 @receiver(post_save, sender=User)
-def designate_person_to_userprofile(sender, instance, **kwargs):
+def configure_person_and_userprofile(sender, instance, **kwargs):
     '''When a Person or User is saved, check if they can be matched by email'''
 
     if sender is Person and instance.email:
@@ -18,32 +20,50 @@ def designate_person_to_userprofile(sender, instance, **kwargs):
             user = User.objects.get(email=instance.email)
             person = instance
         except User.DoesNotExist:
+            # There is no user, thus no userprofile to configure. We're done here.
             return
 
-    elif sender is User and instance.email:
-        try:
-            user = instance
-            person = Person.objects.get(email=instance.email)
-        except Person.DoesNotExist:
-            return
+    elif sender is User:
+        user = instance
+        person = None
+        if instance.email:
+            try:
+                person = Person.objects.get(email=instance.email)
+            except Person.DoesNotExist:
+                pass
     else:
+        # Shouldn't happen, but whatever.
         return
 
+    # At this point, the user object is guaranteed to exist.
+
+    # Ensure the existence of a userprofile.
     try:
-        UserProfile.objects.get(user_id=user.id)
+        userprofile = UserProfile.objects.get(user_id=user.id)
     except UserProfile.DoesNotExist:
-        user.userprofile = UserProfile(user_id=user.id)
+        userprofile = UserProfile(user_id=user.id)
 
-    user.userprofile.person = person
-    user.userprofile.name = person.name
-    user.userprofile.initials = user.userprofile.person.seats.last().name_abbreviation
-    user.userprofile.save()
+    # Configure userprofile according to information about the associated person.
+    if person is not None:
+        last_seat = person.seats.select_related('party').last()
 
+        userprofile.person = person
+        userprofile.name = person.name
+        userprofile.initials = last_seat.name_abbreviation
 
-@receiver(post_save, sender=User)
-def ensure_userprofile(sender, instance, **kwargs):
-    try:
-        userprofile = instance.userprofile
-    except UserProfile.DoesNotExist:
-        instance.userprofile = UserProfile(user_id=instance.id)
-        instance.userprofile.save()
+        # Get the group corresponding to the party, if it exists. Otherwise,
+        # create it, and add the user to it. This ensures that people in the
+        # same party all belong to the same group.
+        group, created = Group.objects.get_or_create(name=last_seat.party.name)
+        group.user_set.add(user)
+
+        # Automatically give access to everyone in the party group. Note that
+        # a user can choose to strip access from his own group by removing the
+        # full access.
+        access, created = Access.objects.get_or_create(user_id=user.id, friend_group_id=group.id)
+        if created:
+            access.full_access = True
+            access.save()
+
+    # Finally, save the profile.
+    userprofile.save()

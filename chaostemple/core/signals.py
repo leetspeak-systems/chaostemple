9d5fe6dd9c21f8e7a954d7d7c8d40935c0019739
Helgi Hrafn Gunnarsson <helgi@binary.is>
2018-05-05 17:50:34 +0000

    Data management tool for dealing with integrity issues added.

--- chaostemple/core/signals.py
+++ chaostemple/core/signals.py
@@ -1,6 +1,8 @@
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 
+from althingi.exceptions import DataIntegrityException
+
 from althingi.models import Document
 from althingi.models import Review
 
@@ -19,5 +21,56 @@ def delete_dossiers_on_deletion(sender, instance, **kwargs):
 
     dossiers = Dossier.objects.filter(**dossier_kwargs)
     for dossier in dossiers:
-        if not dossier.is_useful():
+        if dossier.is_useful():
+            if sender is Review:
+                # This is a review that is about to be deleted, but it has a
+                # user-generated dossier attached to it with useful
+                # information, which we shouldn't delete. Instead, we'll throw
+                # an exception with enough information included for an
+                # administrator or developer to handle the problem as quickly
+                # and easily as possible.
+
+                # Sometimes reviews are deleted and added again for some
+                # reason. One relatively common reason is that its sender
+                # sends in a new review with additional information. Such
+                # reviews are not updated (and possibly cannot be, due to
+                # formalities in their processing) but rather their old
+                # version is removed and the new version added. Such reviews
+                # will be from the same sender and be of the same type, but
+                # their dates may differ. However, the same sender may also
+                # send in more than one review with the same type, so we can't
+                # trust that such reviews are the same as the one being
+                # deleted. Thus the matter must be resolved with manual
+                # intervention.
+
+                # If there is another review in the same issue with the same
+                # sender and type, well want to mention that in our exception.
+
+                review = dossier.review
+
+                msg = 'Useful dossier found when trying to delete review.'
+                msg += ' User: %s.' % dossier.user
+                msg += ' Sender: %s.' % review.sender_name
+                msg += ' Review log_num: %d.' % review.log_num
+                msg += ' Issue: %d/%d.' % (
+                    review.issue.issue_num,
+                    review.issue.parliament.parliament_num
+                )
+
+                potential_replacements = Review.objects.filter(
+                    issue_id=review.issue_id,
+                    review_type=review.review_type,
+                    sender_name=review.sender_name
+                ).exclude(id=review.id)
+
+                if len(potential_replacements):
+                    log_nums = [str(r.log_num) for r in potential_replacements]
+                    msg += ' Potential replacements (log_nums): %s.' % ', '.join(log_nums)
+                else:
+                    msg += ' No potential replacements found.'
+
+                raise DataIntegrityException(msg)
+
+        else:
+            # We can safely delete this dossier, since it's not useful.
             dossier.delete()
---
+++ chaostemple/dossier/management/commands/move_dossiers.py
@@ -0,0 +1,51 @@
+from django.core.management.base import BaseCommand
+
+from sys import stdout
+
+from althingi.althingi_settings import CURRENT_PARLIAMENT_NUM
+
+from althingi.models import Review
+
+from dossier.models import Dossier
+
+class Command(BaseCommand):
+    help = 'Safely moves dossiers from one review to another.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('source_review_log_num', nargs=1, type=int)
+        parser.add_argument('target_review_log_num', nargs=1, type=int)
+        parser.add_argument('parliament_num', nargs='?', type=int)
+
+    def handle(self, *args, **options):
+        source_review_log_num = options['source_review_log_num'][0]
+        target_review_log_num = options['target_review_log_num'][0]
+        parliament_num = options['parliament_num'] or CURRENT_PARLIAMENT_NUM
+
+        source = Review.objects.select_related('issue').get(
+            log_num=source_review_log_num,
+            issue__parliament__parliament_num=parliament_num
+        )
+
+        target = Review.objects.get(
+            log_num=target_review_log_num,
+            issue__parliament__parliament_num=parliament_num
+        )
+
+        for dossier in source.dossiers.select_related('user').all():
+            stdout.write('Moving dossier for user %s...' % dossier.user)
+            stdout.flush()
+
+            dossier.review_id = target.id
+            dossier.save()
+
+            stdout.write(' done\n')
+
+        # Then, let's calculate all the dossier statistics again.
+        # Slowly, as requested.
+        for stat in source.issue.dossierstatistic_set.select_related('user').all():
+            stdout.write('Recalculating dossier statistics for user %s...' % stat.user)
+            stdout.flush()
+
+            stat.update_stats_quite_inefficiently_please()
+
+            stdout.write(' done\n')

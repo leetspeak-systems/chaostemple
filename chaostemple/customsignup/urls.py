2886803d8f7db591ccc756748fc20f05721f5b23
Helgi Hrafn Gunnarsson <helgi@binary.is>
2019-06-14 11:32:06 +0000

    Changes for compatibility with Django 2.1.x.

--- chaostemple/customsignup/urls.py
+++ chaostemple/customsignup/urls.py
@@ -13,15 +13,15 @@ from customsignup.views import custom_profile_data
 
 urlpatterns = [
     url(r'^login/$', CustomLoginView.as_view(), name='login'),
-    url(r'^accounts/logout/$', auth_views.logout, {'next_page': settings.LOGIN_URL}),
+    url(r'^accounts/logout/$', auth_views.LogoutView.as_view(), {'next_page': settings.LOGIN_URL}),
     url(r'^register/$', CustomRegistrationView.as_view(), name='registration_register'),
     url(r'^custom-profile-data/$', custom_profile_data, name='custom_profile_data'),
     url(r'^activate/complete/$', terms_required(TemplateView.as_view(template_name='registration/activation_complete.html')), name='registration_activation_complete'),
     url(r'^', include('registration.backends.default.urls')),
 
-    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
-    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^password_reset/$', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.password_reset_confirm, name='password_reset_confirm'),
-    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+        auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]

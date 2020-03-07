df74f2251ab582a1ec5a6b451b86cd9c83c21424
Helgi Hrafn Gunnarsson <helgi@binary.is>
2020-03-07 12:30:14 +0000

    Review PDF proxy view moved to `althingi` module.

--- chaostemple/althingi/urls.py
+++ chaostemple/althingi/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import url
 from django.urls import path
 
 from althingi import dataviews
+from althingi import views
 
 urlpatterns = [
     path('calendars/', dataviews.calendars, name='calendars'),
     path('calendars/ical/', dataviews.ical, name='ical'),
     path('parliament/<int:parliament_num>/issues/csv/', dataviews.csv_parliament_issues, name='csv_parliament_issues'),
+    path('parliament/<int:parliament_num>/review/<int:log_num>/', views.parliament_review, name='parliament_review'),
 ]
-
--- chaostemple/core/views.py
+++ chaostemple/core/views.py
@@ -797,28 +797,6 @@ def parliament_stats(request, parliament_num):
     return render(request, 'core/parliament_stats.html', ctx)
 
 
-def parliament_review(request, parliament_num, log_num):
-    '''
-    A proxy function that will retrieve remote PDF content and pass it
-    through, byte-for-byte. This is done to avoid cross-origin issues. In the
-    future it may also support some kind of auto-download feature, where we
-    download the PDF the first time it is accessed but retrieved from local
-    disk in subsequent requests.
-    '''
-    review = get_object_or_404(Review, issue__parliament__parliament_num=parliament_num, log_num=log_num)
-
-    # Check if we have a copy on the local disk.
-    if review.pdf_filename:
-        filepath = 'althingi/static/%s' % review.pdf_filename
-        with open(filepath, 'rb') as f:
-            content = f.read()
-    else:
-        response = requests.get(review.pdf_link())
-        content = response.content
-
-    return HttpResponse(content, content_type='application/pdf')
-
-
 def parliament_missing_data(request):
     # If the requested parliament exists now, we'll redirect to the front page.
     if request.extravars['parliament']:

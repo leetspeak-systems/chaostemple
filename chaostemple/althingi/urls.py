99ac21228e595c25938c406fdc355d0774f112c6
Helgi Hrafn Gunnarsson <helgi@binary.is>
2020-03-07 12:54:20 +0000

    Althingi module: PDF proxy view supports documents.

--- chaostemple/althingi/urls.py
+++ chaostemple/althingi/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('calendars/', dataviews.calendars, name='calendars'),
     path('calendars/ical/', dataviews.ical, name='ical'),
     path('parliament/<int:parliament_num>/issues/csv/', dataviews.csv_parliament_issues, name='csv_parliament_issues'),
-    path('parliament/<int:parliament_num>/review/<int:log_num>/', views.parliament_review, name='parliament_review'),
+    path('parliament/<int:parliament_num>/document/<int:doc_num>/', views.content_proxy_view, name='parliament_document'),
+    path('parliament/<int:parliament_num>/review/<int:log_num>/', views.content_proxy_view, name='parliament_review'),
 ]
--- chaostemple/althingi/views.py
+++ chaostemple/althingi/views.py
@@ -1,24 +1,48 @@
+from althingi.models import Document
 from althingi.models import Review
+from django.http import Http404
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 
-def parliament_review(request, parliament_num, log_num):
+def content_proxy_view(request, parliament_num, doc_num=None, log_num=None):
     '''
     A proxy function that will retrieve remote PDF content and pass it
-    through, byte-for-byte. This is done to avoid cross-origin issues. In the
-    future it may also support some kind of auto-download feature, where we
-    download the PDF the first time it is accessed but retrieved from local
+    through, byte-for-byte. This may be done to avoid cross-origin issues when
+    displaying content (documents or reviews) in an iframe, for example. In
+    the future it may also support some kind of auto-download feature, where
+    we download the PDF the first time it is accessed but retrieved from local
     disk in subsequent requests.
     '''
-    review = get_object_or_404(Review, issue__parliament__parliament_num=parliament_num, log_num=log_num)
+
+    if doc_num is not None:
+        # We are retrieving a `document`.
+        document = get_object_or_404(
+            Document,
+            issue__parliament__parliament_num=parliament_num,
+            doc_num=doc_num
+        )
+
+        pdf_filename = document.pdf_filename
+        pdf_link = document.pdf_link
+
+    elif log_num is not None:
+        # We are retrieving a `review`.
+        review = get_object_or_404(
+            Review,
+            issue__parliament__parliament_num=parliament_num,
+            log_num=log_num
+        )
+
+        pdf_filename = review.pdf_filename
+        pdf_link = review.pdf_link
 
     # Check if we have a copy on the local disk.
-    if review.pdf_filename:
-        filepath = 'althingi/static/%s' % review.pdf_filename
+    if pdf_filename:
+        filepath = 'althingi/static/%s' % pdf_filename
         with open(filepath, 'rb') as f:
             content = f.read()
     else:
-        response = requests.get(review.pdf_link())
+        response = requests.get(pdf_link())
         content = response.content
 
     return HttpResponse(content, content_type='application/pdf')

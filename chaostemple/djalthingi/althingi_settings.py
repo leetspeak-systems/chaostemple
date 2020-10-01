ec3dd873c3c542f5b46c1537503d7c4d71f083b0
Helgi Hrafn Gunnarsson <helgi@binary.is>
2020-10-01 18:13:05 +0000

    Parliament number increased to 151.

--- chaostemple/djalthingi/althingi_settings.py
+++ chaostemple/djalthingi/althingi_settings.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 FIRST_PARLIAMENT_NUM = 20
 
-CURRENT_PARLIAMENT_NUM = 150
+CURRENT_PARLIAMENT_NUM = 151
 
 STATIC_DOCUMENT_DIR = os.path.join(BASE_DIR, 'djalthingi/static')
 XML_CACHE_DIR = os.path.join(BASE_DIR, 'djalthingi/xmlcache')
